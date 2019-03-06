4372e85f38ce059a95142edc1558d13f918c8ff3
jbloom <jbloom@fredhutch.org>
2019-03-06 05:53:48 -0800

    clean run of notebooks after changes

--- run_analysis.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-jupyter nbconvert --to notebook --execute --inplace --ExecutePreprocessor.timeout=-1 align_and_annotate.ipynb
-jupyter nbconvert --to notebook --execute --inplace --ExecutePreprocessor.timeout=-1 pacbio_analysis.ipynb
-jupyter nbconvert --to notebook --execute --inplace --ExecutePreprocessor.timeout=-1 monocle_analysis.ipynb
